e319fb17dc9ef6eef8ec488e6d7f546d847fc8a7
DashingQi <dashingqi@gmail.com>
2020-05-05 22:24:41 +0800

    [update]

--- library-service/src/main/java/com/dashingqi/library/service/providers/wx/WxProvider.kt
+++ library-service/src/main/java/com/dashingqi/library/service/providers/wx/WxProvider.kt
@@ -1,4 +1,4 @@
-package com.dashingqi.library.service.providers.article
+package com.dashingqi.library.service.providers.wx
 
 import com.alibaba.android.arouter.facade.template.IProvider
 
@@ -7,5 +7,5 @@ import com.alibaba.android.arouter.facade.template.IProvider
  * @time : 2020/4/29
  * desc :
  */
-interface ArticleProvider : IProvider {
+interface WxProvider : IProvider {
 }
