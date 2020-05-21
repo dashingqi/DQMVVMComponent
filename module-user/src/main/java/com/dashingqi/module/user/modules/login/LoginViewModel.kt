bd21e9d1ae2305b33eceb83297a325007871d796
DashingQi <dashingqi@gmail.com>
2020-05-21 17:38:33 +0800

    [完成登录布局]

---
+++ module-user/src/main/java/com/dashingqi/module/user/modules/login/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.dashingqi.module.user.modules.login
+
+import android.app.Application
+import com.dashingqi.base.base.viewmodel.BaseViewModel
+
+/**
+ * @author : zhangqi
+ * @time : 2020/5/21
+ * desc :
+ */
+class LoginViewModel(application: Application):BaseViewModel(application) {
+}
