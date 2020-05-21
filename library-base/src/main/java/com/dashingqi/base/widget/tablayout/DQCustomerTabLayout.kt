316d2a1cd56a4b75a8ab5e4817625fa4df783b10
DashingQi <dashingqi@gmail.com>
2020-05-21 11:11:25 +0800

    [解决65535的问题]

---
+++ library-base/src/main/java/com/dashingqi/base/widget/tablayout/DQCustomerTabLayout.kt
@@ -0,0 +1,61 @@
+package com.dashingqi.base.widget.tablayout
+
+import android.content.Context
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.provider.CalendarContract
+import android.util.AttributeSet
+import android.widget.TextView
+import com.dashingqi.library_base.R
+import com.google.android.material.tabs.TabLayout
+
+/**
+ * @author : zhangqi
+ * @time : 2020/5/21
+ * desc :自定义的TabLayout
+ * 目前主要就是为了设置一个自定义的指示器
+ */
+class DQCustomerTabLayout : TabLayout {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(context, attributeSet, defStyleAttr)
+
+    init {
+        addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabReselected(tab: Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabUnselected(tab: Tab?) {
+                //当没有被选中，设置文字的样式
+                tab?.customView?.findViewById<TextView>(R.id.customerTableLayoutTitle)?.let {
+                    it.textSize = 14F
+                    it.typeface = Typeface.DEFAULT_BOLD
+                }
+            }
+
+            override fun onTabSelected(tab: Tab?) {
+                //当被选中，设置文字的样式
+                tab?.customView?.findViewById<TextView>(R.id.customerTableLayoutTitle)?.let {
+                    it.textSize = 16F
+                    it.typeface = Typeface.DEFAULT
+                }
+            }
+        })
+
+        //设置指示器的样式
+        setSelectedTabIndicator(R.drawable.base_shape_customer_tl_indicator)
+    }
+
+    /**
+     * 自定义Tab
+     */
+    override fun newTab(): Tab {
+        val tab = super.newTab()
+        tab.setCustomView(R.layout.base_tab_layout_item)
+        tab.customView?.let { findViewById<TextView>(R.id.customerTableLayoutTitle).setTextColor(tabTextColors) }
+        return tab
+    }
+
+
+}
---
+++ module-user/src/main/java/com/dashingqi/module/user/other/UserTabLayoutTestActivity.kt
@@ -0,0 +1,13 @@
+package com.dashingqi.module.user.other
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dashingqi.module.user.R
+
+class UserTabLayoutTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
