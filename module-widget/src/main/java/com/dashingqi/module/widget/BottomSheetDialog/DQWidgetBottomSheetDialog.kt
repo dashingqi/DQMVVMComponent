9835d550d2d05f489d8d5768a98dedf9be02a5a6
dashignqi <1415942241@qq.com>
2020-06-10 23:40:32 +0800

    [update]

--- module-widget/src/main/java/com/dashingqi/module/widget/BottomSheetDialog/DQWidgetBottomSheetDialog.kt
+++ module-widget/src/main/java/com/dashingqi/module/widget/BottomSheetDialog/DQWidgetBottomSheetDialog.kt
@@ -1,12 +1,18 @@
 package com.dashingqi.module.widget.BottomSheetDialog
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dashingqi.module.widget.R
 import com.dashingqi.module.widget.databinding.WidgetBottomSheetDialogLayoutBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
 
 /**
  *
@@ -24,13 +30,54 @@ class DQWidgetBottomSheetDialog:BottomSheetDialogFragment() {
 
     var list = ArrayList<String>()
 
+    private var mBottomSheetBehavior: BottomSheetBehavior<View>? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var viewBinding = WidgetBottomSheetDialogLayoutBinding.inflate(inflater, container, false)
         initData()
         viewBinding.rv.layoutManager = LinearLayoutManager(context)
         viewBinding.rv.adapter = BottomSheetDialogAdapter(list)
+
         return viewBinding.root
     }
+
+
+
+    private val mBottomSheetBehaviorCallback: BottomSheetCallback = object : BottomSheetCallback() {
+        override fun onStateChanged(@NonNull bottomSheet: View, newState: Int) {
+            //禁止拖拽，
+            if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                //设置为收缩状态
+                mBottomSheetBehavior!!.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        override fun onSlide(@NonNull bottomSheet: View, slideOffset: Float) {}
+    }
+
+    override fun onStart() {
+        super.onStart()
+//        var myDialog = dialog
+//        if (myDialog!=null){
+//            var bottomSheet = myDialog.findViewById<View>(R.id.design_bottom_sheet)
+//            bottomSheet.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+//        }
+//
+//        var myView = view
+//        myView?.post {
+//            var parent = myView.parent as View
+//            val params = parent.layoutParams as CoordinatorLayout.LayoutParams
+//            val behavior = params.behavior
+//            mBottomSheetBehavior = behavior as BottomSheetBehavior
+//            mBottomSheetBehavior?.setBottomSheetCallback(mBottomSheetBehaviorCallback)
+//            val display = activity!!.windowManager.defaultDisplay
+//            //设置高度
+//            //设置高度
+//            val height = display.height / 2
+//            mBottomSheetBehavior?.setPeekHeight(height)
+//        }
+    }
+
 
     private fun initData(){
         for (index in 0..30){
