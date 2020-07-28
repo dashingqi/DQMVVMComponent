279de62ae01c9cd042789ad153319bfeaeeec944
DashingQi <dashingqi@gmail.com>
2020-07-28 08:41:40 +0800

    [update]

--- module-login/src/main/java/com/dashingqi/module/login/modules/LoginViewModel.kt
+++ module-login/src/main/java/com/dashingqi/module/login/modules/LoginViewModel.kt
@@ -2,10 +2,12 @@ package com.dashingqi.module.login.modules
 
 import android.app.Application
 import androidx.databinding.ObservableField
+import com.alibaba.android.arouter.launcher.ARouter
 import com.dashingqi.base.base.callback.LiveDataCallback
 import com.dashingqi.base.base.response.BaseResponse
 import com.dashingqi.base.base.viewmodel.BaseViewModel
 import com.dashingqi.base.utils.toast
+import com.dashingqi.library.service.providers.user.UserService
 import com.dashingqi.module.login.net.ILoginService
 import com.dashingqi.module.login.net.LoginResponse
 import com.orhanobut.logger.Logger
@@ -43,10 +45,17 @@ class LoginViewModel(application: Application) : BaseViewModel(application) {
         ILoginService.instance.login(userNameField.get()!!, passwordField.get()!!).enqueue(LiveDataCallback<LoginResponse>(baseLiveData)
                 .bindLoading()
                 .doOnResponseSuccess { _, response ->
+                    var userData = response.data
+                    //存储一下当前登录获取到用户数据
+                    ARouter.getInstance().navigation(UserService::class.java).setUserData(userData)
+                    //登录成功后能拿到当前收藏文章的id，需要处理一下当前收藏文章
+                    var collectIds = userData.collectIds
+                    if (collectIds.isNotEmpty()) {
 
+                    }
                     baseLiveData.finish()
                 }
-                .doOnResponseCodeError { call, response ->
+                .doOnResponseCodeError { _, response ->
                     toast(response.errorMsg)
                 }
 
