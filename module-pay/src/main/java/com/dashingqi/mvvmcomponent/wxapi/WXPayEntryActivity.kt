d9ad385321f857500e1069b87f9c59ff85051190
DashingQi <m13478493231@163.com>
2020-08-06 14:55:37 +0800

    [update]

---
+++ module-pay/src/main/java/com/dashingqi/mvvmcomponent/wxapi/WXPayEntryActivity.kt
@@ -0,0 +1,11 @@
+package com.dashingqi.mvvmcomponent.wxapi
+
+import com.dashingqi.wxpay.wxapi.WXPayActivity
+
+/**
+ * @author : zhangqi
+ * @time : 2020/8/6
+ * desc :
+ */
+class WXPayEntryActivity : WXPayActivity() {
+}
